1e66c688e9f43e9f6d7f95034913cc9c2f617060
zhouxinrui <1352492197@qq.com>
2022-08-11 11:04:44 +0800

    update

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,12 +1,36 @@
 <template>
     <div>
-        我是首页
+        <!-- 三级联动全局组件，已经注册为全局组件，不需要再引入 -->
+        <TypeNav/>
+        <ListContainer/>
+        <TodayRecommend/>
+        <Rank/>
+        <Like/>
+        <Floor/>
+        <Floor/>
+        <Brand/>
     </div>
 </template>
 
 <script>
+    //引入其余组件
+    import ListContainer from "@/pages/Home/ListContainter"
+    import TodayRecommend from "@/pages/Home/TodayRecommend"
+    import Rank from "@/pages/Home/Rank"
+    import Like from "@/pages/Home/Like"
+    import Floor from "@/pages/Home/Floor"
+    import Brand from "@/pages/Home/Brand"
+   
     export default {
-        name:'',
+        name: "",
+        components:{
+            ListContainer,
+            TodayRecommend,
+            Rank,
+            Like,
+            Floor,
+            Brand
+        }
     }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,32 @@ import Search from '@/pages/Search'
 import Login from '@/pages/Login'
 import Register from '@/pages/Register'
 
+//先把vueRouter原型对象的push，保存一份
+let originPush = VueRouter.prototype.push
+let originReplace = VueRouter.prototype.replace
+
+//重写push|replace
+//第一个参数：告诉原来的push方法，跳转位置、传递参数
+//第二个参数：成功回调
+//第三个参数：失败回调
+VueRouter.prototype.push = function(location,resolve,reject){
+    if(resolve && reject){
+        //call与apply区别
+        //相同点：都可以调用函数、篡改函数的上下文一次
+        //区别：call传递参数用逗号隔开，apply方法传递数组
+        originPush.call(this,location,resolve,reject)
+    }else{
+        originPush.call(this,location,()=>{},()=>{})
+    }
+}
+VueRouter.prototype.replace = function(location,resolve,reject){
+    if(resolve && reject){
+        originReplace.call(this,location,resolve,reject)
+    }else{
+        originReplace.call(this,location,()=>{},()=>{}) 
+    }
+}
+
 //配置路由
 export default new VueRouter({
     routes: [
